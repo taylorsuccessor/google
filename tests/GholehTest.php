57009104fde951750b9a03b25ac8347d6a2cc36c
mohammad <mohammadgholeh@gmail.com>
2019-02-28 19:22:30 +0200

    gholeh test

---
+++ tests/GholehTest.php
@@ -0,0 +1,43 @@
+<?php
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+
+use App\controller\Google;
+
+use App\controller\Hashim;
+
+final class GholehTest extends TestCase
+{
+
+
+    public   function testGoogle_1(){
+
+        print_r($this->getFileArray(__DIR__.'/c_medium.in'));
+        $google=new Google();
+        $google->test();
+        $this->assertTrue(Config::get('true')) ;
+        $this->assertEquals(1,1);
+
+    }
+
+    public function getFileArray($fileName){
+
+        $fileContent = file_get_contents($fileName);
+
+        $fileRows= explode("\n",$fileContent);
+        $rowsArray=[];
+        foreach($fileRows as $rowIndex=>$row){
+
+            if($rowIndex ==0 || strlen($row)<2) continue;
+
+            $rowsArray[] = str_split($row);
+        }
+        return $rowsArray;
+
+
+    }
+
+}
+
